bf72413665059b3c79e25fae0bd0253ccd8d6d02
MichaelHouvardas <129966368+MichaelHouvardas@users.noreply.github.com>
2024-11-05 01:12:38 -0500

    Update FastelytraClient.java

--- src/client/java/org/fastelytra/fastelytra/client/FastelytraClient.java
+++ src/client/java/org/fastelytra/fastelytra/client/FastelytraClient.java
@@ -3,10 +3,13 @@ package org.fastelytra.fastelytra.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Vec3d;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.util.InputUtil;
 
 public class FastelytraClient implements ClientModInitializer {
+
+    private boolean jumpKeyPreviouslyPressed = false; // Track the state of the jump key
 
     @Override
     public void onInitializeClient() {
@@ -24,6 +27,23 @@ public class FastelytraClient implements ClientModInitializer {
                             player.getRotationVector().z * 0.05
                     );
                 }
+
+                // Check if the player is flying with an elytra and presses the jump key
+                boolean jumpKeyPressed = minecraftClient.options.jumpKey.isPressed();
+
+                if (player.isFallFlying() && jumpKeyPressed && !jumpKeyPreviouslyPressed) {
+                    // Make the player stop using the elytra if the jump key was just pressed
+                    player.stopFallFlying();
+                }
+
+                // Allow the player to use the elytra again if they are in the air and press the jump key
+                if (!player.isFallFlying() && !player.isOnGround() && jumpKeyPressed && !jumpKeyPreviouslyPressed) {
+                    // Start using the elytra
+                    player.startFallFlying();
+                }
+
+                // Update the previous state of the jump key
+                jumpKeyPreviouslyPressed = jumpKeyPressed;
             }
         });
     }
